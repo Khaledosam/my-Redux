858fa60030dbc1d4560180374ce3611f020a1310
khaled osama <khaledosamakamel82@gmail.com>
2024-09-20 17:14:12 +0200

    redux-one

--- src/features/counter/counter.jsx
+++ src/features/counter/counter.jsx
@@ -8,31 +8,51 @@ const Counter = () => {
   const [incrementAmount, setIncrementAmount] = useState(0);
   const addValue = Number(incrementAmount) || 0;
   const resetAll = () => {
-    setIncrementAmount(0);
-    dispatch(reset());
+    setIncrementAmount(0), dispatch(reset(0));
   };
   return (
-    <section>
-      <p>{count}</p>
-      <div>
-        <button onClick={() => dispatch(increment())}>+</button>
-        <button onClick={() => dispatch(decrement())}>-</button>
+    <section
+      style={{
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        alignItems: "center",
+      }}
+    >
+      <p style={{ fontSize: "40px" }}>{count}</p>
+      <div style={{ display: "flex", gap: "20px" }}>
+        <button
+          style={{ color: "black", backgroundColor: "orange" }}
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+        <button
+          style={{ color: "black", backgroundColor: "orange" }}
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
       </div>
       <input
+        style={{ textAlign: "center", fontSize: "20px" }}
         type="text"
         value={incrementAmount}
         onChange={(e) => setIncrementAmount(e.target.value)}
       />
-      <div>
-        <button
-          onClick={() => {
-            dispatch(incrementByAmount(addValue));
-          }}
-        >
-          Add Amount
-        </button>
-        <button onClick={resetAll}>Reset</button>
-      </div>
+      <button
+        style={{ color: "black", backgroundColor: "orange" }}
+        onClick={() => dispatch(incrementByAmount(addValue))}
+      >
+        Add Amount
+      </button>
+      <button
+        style={{ color: "black", backgroundColor: "orange" }}
+        onClick={() => dispatch(resetAll)}
+      >
+        Reset
+      </button>
     </section>
   );
 };
